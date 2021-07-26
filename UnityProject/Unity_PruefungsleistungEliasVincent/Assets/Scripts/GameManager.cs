4992ddd4d64c4d20ee8e6a71a7aab703c086993c
EliasTechnik <83211893+EliasTechnik@users.noreply.github.com>
2021-07-26 21:13:37 +0200

    KI initialisiert mit bisher 8 Features.

--- UnityProject/Unity_PruefungsleistungEliasVincent/Assets/Scripts/GameManager.cs
+++ UnityProject/Unity_PruefungsleistungEliasVincent/Assets/Scripts/GameManager.cs
@@ -14,11 +14,163 @@ public class GameManager : MonoBehaviour {
 
     public bool respawnbool;
 
+    //KI Start
+    AIActionInterface actionInterface;
+    AIFeature Wall_0;
+    AIFeature Wall_45;
+    AIFeature Wall_90;
+    AIFeature Wall_135;
+    AIFeature Wall_180;
+    AIFeature Wall_225;
+    AIFeature Wall_270;
+    AIFeature Wall_315;
+    AIFeature TargetDistance;
+    FeatureBuilder fb;
+    RewardMatrix rewMatrix;
+    QAgent agent;
+    AIFeatureInterface ai_input;
+
+    //KI End
+
     private void Awake() {
         Instance = this;
     }
 
     private void Start() {
+        //KI instanziieren
+        actionInterface=new AIActionInterface();
+        actionInterface.addAction("W"); //ID=0
+        actionInterface.addAction("A"); //ID=1
+        actionInterface.addAction("S"); //ID=2
+        actionInterface.addAction("D"); //ID=3
+
+        InputRange[] IRdistance=new InputRange[]{
+                new InputRange(0,3,true), //Weit weg
+                new InputRange(1,2,3),    //näher dran  
+                new InputRange(2,0,1)     //nah dran  
+            };
+
+        Wall_0=new AIFeature(//frontal
+            new double[3][]{//W A S D
+                new double[]{1,1,1,1},
+                new double[]{-2,1,1,1},
+                new double[]{-10,1,1,1},
+            },
+            IRdistance,
+            actionInterface,
+            "Wall_0"
+        );
+
+        Wall_45=new AIFeature(//frontal links
+            new double[3][]{//W A S D
+                new double[]{1,1,1,1},
+                new double[]{-1,-1,1,1},
+                new double[]{-5,-5,1,1},
+            },
+            IRdistance,
+            actionInterface,
+            "Wall_45"
+        );
+
+        Wall_90=new AIFeature(//links
+            new double[3][]{//W A S D
+                new double[]{1,1,1,1},
+                new double[]{1,-2,1,1},
+                new double[]{1,-10,1,1},
+            },
+            IRdistance,
+            actionInterface,
+            "Wall_90"
+        );
+
+        Wall_135=new AIFeature(//hinten links
+            new double[3][]{//W A S D
+                new double[]{1,1,1,1},
+                new double[]{1,-1,-1,1},
+                new double[]{1,-5,-5,1},
+            },
+            IRdistance,
+            actionInterface,
+            "Wall_135"
+        );
+
+        Wall_180=new AIFeature(//hinten
+            new double[3][]{//W A S D
+                new double[]{1,1,1,1},
+                new double[]{1,1,-2,1},
+                new double[]{1,1,-10,1},
+            },
+            IRdistance,
+            actionInterface,
+            "Wall_180"
+        );
+        Wall_225=new AIFeature(//hinten rechts
+            new double[3][]{//W A S D
+                new double[]{1,1,1,1},
+                new double[]{1,1,-1,-1},
+                new double[]{1,1,-5,-5},
+            },
+            IRdistance,
+            actionInterface,
+            "Wall_225"
+        );
+        Wall_270=new AIFeature(//rechts
+            new double[3][]{//W A S D
+                new double[]{1,1,1,1},
+                new double[]{1,1,1,-2},
+                new double[]{1,1,1,-10},
+            },
+            IRdistance,
+            actionInterface,
+            "Wall_270"
+        );
+        Wall_315=new AIFeature(//frontal rechts
+            new double[3][]{//W A S D
+                new double[]{1,1,1,1},
+                new double[]{-1,1,1,-1},
+                new double[]{-5,1,1,-5},
+            },
+            IRdistance,
+            actionInterface,
+            "Wall_315"
+        );
+
+        fb=new FeatureBuilder(
+            Wall_315,
+            new FeatureBuilder(
+                Wall_270,
+                new FeatureBuilder(
+                    Wall_225,
+                    new FeatureBuilder(
+                        Wall_180,
+                        new FeatureBuilder(
+                            Wall_135,
+                            new FeatureBuilder(
+                                Wall_90,
+                                new FeatureBuilder(
+                                    Wall_45,
+                                    new FeatureBuilder(
+                                        Wall_0
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        );
+        Debug.Log("Feature Combinations: "+fb.CombinationCount.ToString());
+
+        rewMatrix=new RewardMatrix(fb);
+        rewMatrix.generateMatrix();
+
+        ai_input=new AIFeatureInterface();
+        ai_input=fb.getFeatureInterface(ai_input);
+
+        agent=new QAgent(rewMatrix, actionInterface);
+        
+        //KI Ende
+
         Player = new PlayerObject(GameObject.Find("player_obj"));
         Player.RespawnPlayer();
         obstacleGenerator.HandleObstacles();
